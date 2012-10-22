e7803bbb1abe3154d4c7f4b49c68fde31c067136
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-10-21 23:56:07 -0300

    Added docstrings

--- zombsole/core.py
+++ zombsole/core.py
@@ -4,30 +4,36 @@ import os
 
 
 class World(object):
+    '''World where to play the game.'''
     def __init__(self, size):
         self.size = size
         self.terrain = {}
         self.actors = {}
 
     def thing_at(self, x, y):
+        '''Return the thing at the given position.'''
         return self.actors.get((x, y)) or \
                self.terrain.get((x, y)) or \
                ' '
 
     def draw(self):
+        '''Draw the world'''
         os.system('clear')
         print '\n'.join(''.join(self.thing_at(x, y)
                                 for x in xrange(self.size[0]))
                         for y in xrange(self.size[1]))
 
     def t(self):
+        '''Forward one instant of time.'''
         pass
 
 
 def main_loop(world):
+    '''Game main loop.'''
     playing = True
 
     while playing:
         world.t()
         world.draw()
         time.sleep(0.5)
+
