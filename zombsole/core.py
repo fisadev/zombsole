4d6e30a03edde45498b83f6e58b9a53cd6eec67e
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-10-22 01:37:32 -0300

    Added docstring, and different logic on check_free_position

--- zombsole/core.py
+++ zombsole/core.py
@@ -11,7 +11,8 @@ class World(object):
         self.things = {}
 
     def _check_free_position(self, position):
-        if self.things.get(position) is not None:
+        '''Check if position is free.'''
+        if position in self.things:
             raise Exception('two things in the same place!')
 
     def add_thing(self, thing, position):
