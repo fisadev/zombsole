087344e466a90986f83e579d930ec2e4df3dec70
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-10-22 00:09:43 -0300

    Refactor of actors and terrain: now all things. Some things classes

--- zombsole/core.py
+++ zombsole/core.py
@@ -7,23 +7,23 @@ class World(object):
     '''World where to play the game.'''
     def __init__(self, size):
         self.size = size
-        self.terrain = {}
-        self.actors = {}
-
-    def thing_at(self, x, y):
-        '''Return the thing at the given position.'''
-        return self.actors.get((x, y)) or self.terrain.get((x, y))
+        self.things = {}
+        self.t = None
 
     def draw(self):
         '''Draw the world'''
         os.system('clear')
-        return '\n'.join(''.join(str(self.thing_at(x, y))
+        return '\n'.join(''.join(str(self.things.get((x, y), ' '))
                                  for x in xrange(self.size[0]))
                          for y in xrange(self.size[1]))
 
-    def t(self):
+    def time(self):
         '''Forward one instant of time.'''
-        pass
+        if self.t is None:
+            self.t = -1
+        self.t += 1
+        for thing in self.things:
+            thing.time(t)
 
 
 def main_loop(world):
@@ -31,7 +31,27 @@ def main_loop(world):
     playing = True
 
     while playing:
-        world.t()
+        world.time()
         print world.draw()
         time.sleep(0.5)
+
+
+class Thing(object):
+    '''Something in the world.'''
+    def __init__(self, position, world, objetive):
+        self.position = position
+        self.world = world
+        self.objetive = objetive
+        self.t = None
+
+    def time(self, t):
+        '''Forward one instant of time.'''
+        self.t = t
+
+
+class MovingThing(Thing):
+    '''Something that's able to move by it's own.'''
+    def __init__(self, position, world, speed):
+        super(MovingThing, self).__init__(position, world)
+        self.speed = speed
 
