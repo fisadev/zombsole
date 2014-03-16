5a27de9cf7294f0fbff4dcb52f305d71b960c25e
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-16 16:49:28 -0300

    Weapons on a different module

--- zombsole/things.py
+++ zombsole/things.py
@@ -1,7 +1,8 @@
 #coding: utf-8
 import random
-from zombsole.core import Thing, FightingThing, ComplexThingBuilder, Weapon
+from zombsole.core import Thing, FightingThing, ComplexThingBuilder
 from zombsole.utils import closest, distance, possible_moves
+from zombsole.weapons import ZombieClaws, Knife, Axe, Gun, Rifle, Shotgun
 
 
 class Box(Thing):
@@ -60,26 +61,6 @@ class Building(ComplexThingBuilder):
         walls = walls[self.doors:]
 
         return walls
-
-
-def _new_weapon_class(name, max_range, damage_range):
-    '''Create new weapon class.'''
-    class NewWeapon(Weapon):
-        def __init__(self):
-            super(NewWeapon, self).__init__(name,
-                                            max_range,
-                                            damage_range)
-
-    NewWeapon.__name__ = name
-    return NewWeapon
-
-
-ZombieClaws = _new_weapon_class('ZombieClaws', 1.5, (5, 10))
-Gun = _new_weapon_class('Gun', 6, (10, 50))
-Shotgun = _new_weapon_class('Shotgun', 3, (75, 100))
-Rifle = _new_weapon_class('Rifle', 10, (25, 75))
-Knife = _new_weapon_class('Knife', 1.5, (5, 10))
-Axe = _new_weapon_class('Axe', 1.5, (75, 100))
 
 
 class Zombie(FightingThing):
---
+++ zombsole/weapons.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+from zombsole.core import Weapon
+
+
+def _new_weapon_class(name, max_range, damage_range):
+    '''Create new weapon class.'''
+    class NewWeapon(Weapon):
+        def __init__(self):
+            super(NewWeapon, self).__init__(name,
+                                            max_range,
+                                            damage_range)
+
+    NewWeapon.__name__ = name
+    return NewWeapon
+
+
+ZombieClaws = _new_weapon_class('ZombieClaws', 1.5, (5, 10))
+
+Knife = _new_weapon_class('Knife', 1.5, (5, 10))
+Axe = _new_weapon_class('Axe', 1.5, (75, 100))
+
+Gun = _new_weapon_class('Gun', 6, (10, 50))
+Rifle = _new_weapon_class('Rifle', 10, (25, 75))
+Shotgun = _new_weapon_class('Shotgun', 3, (75, 100))
