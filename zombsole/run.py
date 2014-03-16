6693ed10ce1c9d75cf4989095dbbc9587514fc60
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-16 20:17:01 -0300

    Sample run.py working

--- zombsole/run.py
+++ zombsole/run.py
@@ -1,5 +1,15 @@
 #coding: utf-8
 from zombsole.game import Game
+from zombsole.things import Human
 
-g = Game((20, 20), [], None)
-g.play()
+
+
+def run():
+    g = Game(players=[Human('player1', 'blue')],
+             size=(80, 20),
+             initial_zombies=10)
+    g.play()
+
+
+if __name__ == '__main__':
+    run()
