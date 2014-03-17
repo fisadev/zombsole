8211d211d003a59a7aea3908121acf87d3ba71a5
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-16 21:24:06 -0300

    Evacuation requires half of the team evacuated

--- zombsole/game.py
+++ zombsole/game.py
@@ -162,10 +162,13 @@ class SafeHouseGame(Game):
 class EvacuationGame(Game):
     '''A kind of game where players must get together to be evacuated.
 
-       Team wins when all of them are at most at distance=2 of another player.
+       Team wins when all alive players are at 2 or less distance from another
+       alive player, and at least half of the team must survive.
     '''
     def game_ended(self):
-        if not self.players_alive():
+        alive_players = [player for player in self.players
+                         if player.live > 0]
+        if len(alive_players) < len(self.players) / 2:
             return True
         else:
             for player in self.players:
