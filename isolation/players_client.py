6e62242ba384f3f061824c5cfce95bf160a54cc0
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-20 01:00:57 -0300

    No more player creator creator, just a player creator

--- isolation/players_client.py
+++ isolation/players_client.py
@@ -2,7 +2,7 @@
 import requests
 
 
-def player_creator(player_name, port):
-    '''Create a player creator function, which mimicks the real function,
-       but returning a proxy to players served with serve_players.'''
+def create_player_client(player_name, rules_name, objetives, isolator_port):
+    '''Create a proxy which mimicks a player, but calling players on the
+       isolated server.'''
     pass
