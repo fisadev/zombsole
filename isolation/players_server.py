60de472eff9c5b1bde2af5fd29a856f3e16273db
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-20 00:48:27 -0300

    Simple player creation on the players server

--- isolation/players_server.py
+++ isolation/players_server.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python
 # coding: utf-8
 '''Serve the players logic as a http service.'''
-from flask import Flask
+import json
+
+from flask import Flask, request
+
+from game import get_creator
 
 
 app = Flask('zombsole_isolator')
+players = {}
 
 
-@app.route('/')
-def index():
+@app.route('/create_player')
+def create_player():
+    player_name = request.form['player_name']
+    rules = request.form['rules']
+    objetives = json.loads(request.form['objetives'])
+
+    player_creator = get_creator('players.' + player_name)
+    player = player_creator(rules, objetives)
+    players[player_name] = player
+
+    return 'ok'
+
+
+@app.route('/next_step')
+def next_step():
     return 'ok'
 
 
