cc7812a517d103c433c3434d29892c46a151625a
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-03-22 14:35:24 -0300

    Added another player example: hamster

---
+++ players/hamster.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import random
+
+from things import Player
+from utils import possible_moves
+
+
+class Hamster(Player):
+    '''A player allways moves.'''
+    def next_step(self, things, t):
+        self.status = u'wii wi wiii'
+        moves = possible_moves(self, things)
+        if moves:
+            return 'move', random.choice(moves)
+
+
+def create(rules, objetives=None):
+    return Hamster('hamster', 'white')
